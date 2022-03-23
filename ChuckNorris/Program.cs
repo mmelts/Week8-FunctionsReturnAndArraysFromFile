46b0dde1307674f425b438346327e5eeeb246a8e
Kristo Toomsoo <kristo@lteed.ee>
2022-03-23 16:54:08 +0200

    add new projects

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomNorris;
+            randomNorris = GetRandomFromFile("chuck.txt");
+            
+            Console.WriteLine($"Here are some famous jokes of Chuck Norris: {randomNorris}");
+        }
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+        private static string GetRandomFromFile(string filename)
+        {
+            string filePath = $@"C:\...\...\...\{fileNames}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+    }
+}
